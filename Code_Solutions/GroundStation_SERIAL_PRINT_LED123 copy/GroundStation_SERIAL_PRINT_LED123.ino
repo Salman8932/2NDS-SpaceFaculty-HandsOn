b4a004f46a42ad2919a7619776a72bd9b8a4d873
rc2nds <161291974+rc2nds@users.noreply.github.com>
2024-03-05 14:42:50 +0800

    Update GroundStation_SERIAL_PRINT_LED123.ino
    Use this solution to understand how to correctly set up the Serial Communication

--- Code_Solutions/GroundStation_SERIAL_PRINT_LED123 copy/GroundStation_SERIAL_PRINT_LED123.ino
+++ Code_Solutions/GroundStation_SERIAL_PRINT_LED123 copy/GroundStation_SERIAL_PRINT_LED123.ino
@@ -1,18 +1,22 @@
-//THIS PROGRAM TURNS ON LED1, LED2 and LED3 ON THE ARDUINO NANO GROUND STATION
-
 #define LED1 4
 #define LED2 6
 #define LED3 8
 
 void setup() {
-pinMode(LED1, OUTPUT);
-pinMode(LED2, OUTPUT);
-pinMode(LED3, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  Serial.begin(9600);  //Initialise the Serial Communication
 }
 
 void loop() {
   digitalWrite(LED1, HIGH);
+  Serial.print("LED1 is ON");
+  delay(500);
   digitalWrite(LED2, HIGH);
+  Serial.print("LED1 is ON");
+  delay(500);
   digitalWrite(LED3, HIGH);
-
+  Serial.print("LED1 is ON");
+  delay(500);
 }
